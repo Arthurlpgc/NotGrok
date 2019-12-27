27aa44966ae98e52efed8e281328762c45d53d13
Arthur Costa <arthurlpgcosta@fb.com>
2019-12-27 23:43:05 +0000

    add params to cli

--- notgrok/client2.py
+++ notgrok/client2.py
@@ -8,30 +8,48 @@ import subprocess
 import sys
 import time
 
+
 class NullDevice():
     def write(self, s):
         pass
 
+
 def main():
     # Discard stderr output
-    # sys.stderr = NullDevice()
+    sys.stderr = NullDevice()
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Does not grok.')
 
     subparsers = parser.add_subparsers(help='Functions')
-    http_parser = subparsers.add_parser('http', help='Set up a subdomain in the remote server.')
-    http_parser.add_argument('-c', '--controllerHostname', help='Hostname of the controller server.', required=True)
-    http_parser.add_argument('-l', '--localPort', type=int, help='Number of the local port.', default=80)
-    http_parser.add_argument('-r', '--remotePort', type=int, help='Number of the remote port.', default=random.randint(10000,60000))
-    http_parser.add_argument('-s', '--subdomain', help='Name of the remote subdomain.', required=True)
+    http_parser = subparsers.add_parser(
+        'http', help='Set up a subdomain in the remote server.')
+    http_parser.add_argument(
+        '-c', '--controllerHostname', help='Hostname of the controller server.', required=True)
+    http_parser.add_argument(
+        '-k', '--sshKey', help='SSH key.', default='./id_rsa')
+    http_parser.add_argument(
+        '-l', '--localPort', type=int, help='Number of the local port.', default=80)
+    http_parser.add_argument(
+        '--localHost', help='Address of the local host.', default='localhost')
+    http_parser.add_argument(
+        '-r', '--remotePort', type=int, help='Number of the remote port.', default=random.randint(10000, 60000))
+    http_parser.add_argument(
+        '-s', '--subdomain', help='Name of the remote subdomain.', required=True)
     http_parser.set_defaults(command='http')
 
-
-    tcp_parser = subparsers.add_parser('tcp', help='Set up a subdomain in the remote server.')
-    tcp_parser.add_argument('-c', '--controllerHostname', help='Hostname of the controller server.', required=True)
-    tcp_parser.add_argument('-l', '--localPort', type=int, help='Number of the local port.', required=True)
-    tcp_parser.add_argument('-r', '--remotePort', type=int, help='Number of the remote port.', required=True)
+    tcp_parser = subparsers.add_parser(
+        'tcp', help='Set up a subdomain in the remote server.')
+    tcp_parser.add_argument(
+        '-c', '--controllerHostname', help='Hostname of the controller server.', required=True)
+    tcp_parser.add_argument(
+        '-k', '--sshKey', help='SSH key.', default='./id_rsa')
+    tcp_parser.add_argument(
+        '-l', '--localPort', type=int, help='Number of the local port.', required=True)
+    tcp_parser.add_argument(
+        '--localHost', help='Address of the local host.', default='localhost')
+    tcp_parser.add_argument(
+        '-r', '--remotePort', type=int, help='Number of the remote port.', required=True)
     tcp_parser.set_defaults(command='tcp')
 
     arguments = parser.parse_args(sys.argv[1:])
@@ -46,7 +64,8 @@ def main():
         "host": arguments.controllerHostname,
         "user": "portforwarding",
     })
-    ssh_binding = "{remote_port}:localhost:{local_port}".format_map({
+    ssh_binding = "{remote_port}:{local_host}:{local_port}".format_map({
+        "local_host": arguments.localHost,
         "local_port": arguments.localPort,
         "remote_port": arguments.remotePort,
     })
@@ -54,10 +73,11 @@ def main():
         while True:
             print("Starting connection...")
             process = subprocess.run(
-                ["ssh", "-p", "722", "-o", "StrictHostKeyChecking=accept-new", "-i", "./id_rsa", "-R", ssh_binding, ssh_target, "-N", "-T", "0.0.0.0"])
+                ["ssh", "-p", "722", "-o", "StrictHostKeyChecking=accept-new", "-i", arguments.sshKey, "-R", ssh_binding, ssh_target, "-N", "-T", "0.0.0.0"])
             time.sleep(1000)
     except KeyboardInterrupt as e:
         print("Exiting...")
+
 
 if __name__ == '__main__':
     main()
